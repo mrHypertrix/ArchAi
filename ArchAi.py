c143b8ee91c999bbb25b117b2d5835ecf81f2417
mrhypertrix <c8822016@gmail.com>
2023-05-10 20:26:24 +0530

    ArchAiTeam💮

---
+++ ArchAi.py
@@ -0,0 +1,343 @@
+from pyrogram import Client, filters
+import asyncio
+from pyrogram.types import *
+from pymongo import MongoClient
+import requests
+import random
+from pyrogram.errors import (
+    PeerIdInvalid,
+    ChatWriteForbidden
+)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from os import getenv
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+SESSION_NAME = getenv("SESSION_NAME", None)
+MONGO_URL = getenv("MONGO_URL", None)
+
+client = Client(SESSION_NAME, API_ID, API_HASH)
+
+async def is_admins(chat_id: int):
+    return [
+        member.user.id
+        async for member in client.iter_chat_members(
+            chat_id, filter="administrators"
+        )
+    ]
+
+@client.on_message(
+    filters.command("repo", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def chatbot(client, message):
+    await message.delete()
+    alexaai = await message.reply("🤭🤏✌️")
+    await asyncio.sleep(1)
+    await alexaai.edit("**🙀🙀🙀**")
+    await asyncio.sleep(1)
+    await alexaai.edit("**ɪ ᴀᴍ ᴅᴏɪɴɢ ᴍʏ ʟᴏᴠᴇ 💕**")
+    await alexaai.delete()
+    await asyncio.sleep(2)
+    umm = await message.reply_sticker("CAACAgUAAx0CWn4axwABBT73ZFujkkbuNfF9q6NTcsFrsidMFagAAo4JAAIIEslWUs-p1XoU0bQvBA")
+    await asyncio.sleep(2)
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/d9ca2d98829cc12c433ba.jpg",
+        caption=f"""━━━━━━━━━━━━━━━━━━━━━━━━
+💥 A ᴘᴏᴡᴇʀғᴜʟ ᴀɪ ʙᴏᴛ
+━━━━━━━━━━━━━━━━━
+ᴅᴀᴛᴀʙᴀsᴇ ʙᴀᴄᴋᴇɴᴅ ʙᴏᴛ ғᴏʀ ᴛɢ...
+┏━━━━━━━━━━━━━━━━━┓
+┣★ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇs [ᴏᴜʀ ᴏᴛʜᴇʀ ʙᴏᴛs](https://t.me/archbots)
+┣★ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://github.com/mrHypertrix/ArchAi)
+┗━━━━━━━━━━━━━━━━━┛
+💞 
+IF HAVE ANY QUESTION THEN CONTACT » TO » MY » [OWNER] @Unknown_hu_m""",
+    ) 
+
+
+@client.on_message(
+    filters.command("alive", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def start(client, message):
+    await message.reply_text(f"**ʜᴇʟʟᴏ ᴋᴇsᴏ ʜᴏ sᴀʙ**")
+
+@client.on_message(
+    filters.command("chatbot off", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def chatbotofd(client, message):
+    archdb = MongoClient(MONGO_URL)    
+    arch = archdb["Archdb"]["Arch"]     
+    if message.from_user:
+        user = message.from_user.id
+        chat_id = message.chat.id
+        if user not in (
+           await is_admins(chat_id)
+        ):
+           return await message.reply_text(
+                ""
+            )
+    is_arch = arch.find_one({"chat_id": message.chat.id})
+    if not is_arch:
+        arch.insert_one({"chat_id": message.chat.id})
+        await message.reply_text(f"ᴄʜᴀᴛʙɪᴛ ɪs ᴅɪsᴀʙʟᴇᴅ ʙʏ {message.from_user.mention()} ғᴏʀ ᴜsᴇʀs ɪɴ {message.chat.title}")
+    if is_arch:
+        await message.reply_text(f"ᴄʜᴀᴛʙɪᴛ ɪs ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ")
+    
+
+@client.on_message(
+    filters.command("chatbot on", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def chatboton(client, message):
+    archdb = MongoClient(MONGO_URL)    
+    arch = archdb["Archdb"]["Arch"]     
+    if message.from_user:
+        user = message.from_user.id
+        chat_id = message.chat.id
+        if user not in (
+            await is_admins(chat_id)
+        ):
+            return await message.reply_text(
+                "ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ"
+            )
+    is_arch = arch.find_one({"chat_id": message.chat.id})
+    if not is_arch:           
+        await message.reply_text(f"ᴄʜᴀᴛʙɪᴛ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ")
+    if is_arch:
+        arch.delete_one({"chat_id": message.chat.id})
+        await message.reply_text(f"ᴄʜᴀᴛʙɪᴛ ɪs ᴇɴᴀʙʟᴇᴅ ʙʏ {message.from_user.mention()} ғᴏʀ ᴜsᴇʀs ɪɴ {message.chat.title}")
+    
+
+@client.on_message(
+    filters.command("chatbot", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def chatbot(client, message):
+    await message.reply_text(f"**ᴜsᴇᴀɢᴇ:**\n/chatbot [on|off] ᴏɴʟʏ ɢʀᴏᴜᴘ")
+
+    
+@client.on_message(
+ (
+        filters.text
+        | filters.sticker
+    )
+    & ~filters.private
+    & ~filters.me
+    & ~filters.bot,
+)
+async def alexaai(client: Client, message: Message):
+
+   chatdb = MongoClient(MONGO_URL)
+   chatai = chatdb["Word"]["WordDb"]   
+
+   if not message.reply_to_message:
+       archdb = MongoClient(MONGO_URL)
+       arch = archdb["Archdb"]["Arch"] 
+       is_arch = arch.find_one({"chat_id": message.chat.id})
+       if not is_arch:
+           await client.send_chat_action(message.chat.id, "typing")
+           K = []  
+           is_chat = chatai.find({"word": message.text})  
+           k = chatai.find_one({"word": message.text})      
+           if k:               
+               for x in is_chat:
+                   K.append(x['text'])          
+               hey = random.choice(K)
+               is_text = chatai.find_one({"text": hey})
+               Yo = is_text['check']
+               if Yo == "sticker":
+                   await message.reply_sticker(f"{hey}")
+               if not Yo == "sticker":
+                   await message.reply_text(f"{hey}")
+   
+   if message.reply_to_message:  
+       archdb = MongoClient(MONGO_URL)
+       arch = archdb["Archdb"]["Arch"] 
+       is_arch = arch.find_one({"chat_id": message.chat.id})    
+       getme = await client.get_me()
+       user_id = getme.id                             
+       if message.reply_to_message.from_user.id == user_id: 
+           if not is_arch:                   
+               await client.send_chat_action(message.chat.id, "typing")
+               K = []  
+               is_chat = chatai.find({"word": message.text})
+               k = chatai.find_one({"word": message.text})      
+               if k:       
+                   for x in is_chat:
+                       K.append(x['text'])
+                   hey = random.choice(K)
+                   is_text = chatai.find_one({"text": hey})
+                   Yo = is_text['check']
+                   if Yo == "sticker":
+                       await message.reply_sticker(f"{hey}")
+                   if not Yo == "sticker":
+                       await message.reply_text(f"{hey}")
+       if not message.reply_to_message.from_user.id == user_id:          
+           if message.sticker:
+               is_chat = chatai.find_one({"word": message.reply_to_message.text, "id": message.sticker.file_unique_id})
+               if not is_chat:
+                   chatai.insert_one({"word": message.reply_to_message.text, "text": message.sticker.file_id, "check": "sticker", "id": message.sticker.file_unique_id})
+           if message.text:                 
+               is_chat = chatai.find_one({"word": message.reply_to_message.text, "text": message.text})                 
+               if not is_chat:
+                   chatai.insert_one({"word": message.reply_to_message.text, "text": message.text, "check": "none"})                                                                                                                                               
+
+@client.on_message(
+ (
+        filters.sticker
+        | filters.text
+    )
+    & ~filters.private
+    & ~filters.me
+    & ~filters.bot,
+)
+async def alexastickerai(client: Client, message: Message):
+
+   chatdb = MongoClient(MONGO_URL)
+   chatai = chatdb["Word"]["WordDb"]   
+
+   if not message.reply_to_message:
+       archdb = MongoClient(MONGO_URL)
+       arch = archdb["Archdb"]["Arch"] 
+       is_arch = arch.find_one({"chat_id": message.chat.id})
+       if not is_arch:
+           await client.send_chat_action(message.chat.id, "typing")
+           K = []  
+           is_chat = chatai.find({"word": message.sticker.file_unique_id})      
+           k = chatai.find_one({"word": message.text})      
+           if k:           
+               for x in is_chat:
+                   K.append(x['text'])
+               hey = random.choice(K)
+               is_text = chatai.find_one({"text": hey})
+               Yo = is_text['check']
+               if Yo == "text":
+                   await message.reply_text(f"{hey}")
+               if not Yo == "text":
+                   await message.reply_sticker(f"{hey}")
+   
+   if message.reply_to_message:
+       archdb = MongoClient(MONGO_URL)
+       arch = archdb["Archdb"]["Arch"] 
+       is_arch = arch.find_one({"chat_id": message.chat.id})
+       getme = await client.get_me()
+       user_id = getme.id
+       if message.reply_to_message.from_user.id == user_id: 
+           if not is_arch:                    
+               await client.send_chat_action(message.chat.id, "typing")
+               K = []  
+               is_chat = chatai.find({"word": message.text})
+               k = chatai.find_one({"word": message.text})      
+               if k:           
+                   for x in is_chat:
+                       K.append(x['text'])
+                   hey = random.choice(K)
+                   is_text = chatai.find_one({"text": hey})
+                   Yo = is_text['check']
+                   if Yo == "text":
+                       await message.reply_text(f"{hey}")
+                   if not Yo == "text":
+                       await message.reply_sticker(f"{hey}")
+       if not message.reply_to_message.from_user.id == user_id:          
+           if message.text:
+               is_chat = chatai.find_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.text})
+               if not is_chat:
+                   toggle.insert_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.text, "check": "text"})
+           if message.sticker:                 
+               is_chat = chatai.find_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.sticker.file_id})                 
+               if not is_chat:
+                   chatai.insert_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.sticker.file_id, "check": "none"})    
+              
+
+
+@client.on_message(
+    (
+        filters.text
+        | filters.sticker
+    )
+    & filters.private
+    & ~filters.me
+    & ~filters.bot,
+)
+async def alexaprivate(client: Client, message: Message):
+
+   chatdb = MongoClient(MONGO_URL)
+   chatai = chatdb["Word"]["WordDb"]
+   if not message.reply_to_message: 
+       await client.send_chat_action(message.chat.id, "typing")
+       K = []  
+       is_chat = chatai.find({"word": message.text})                 
+       for x in is_chat:
+           K.append(x['text'])
+       hey = random.choice(K)
+       is_text = chatai.find_one({"text": hey})
+       Yo = is_text['check']
+       if Yo == "sticker":
+           await message.reply_sticker(f"{hey}")
+       if not Yo == "sticker":
+           await message.reply_text(f"{hey}")
+   if message.reply_to_message:            
+       getme = await client.get_me()
+       user_id = getme.id       
+       if message.reply_to_message.from_user.id == user_id:                    
+           await client.send_chat_action(message.chat.id, "typing")
+           K = []  
+           is_chat = chatai.find({"word": message.text})                 
+           for x in is_chat:
+               K.append(x['text'])
+           hey = random.choice(K)
+           is_text = chatai.find_one({"text": hey})
+           Yo = is_text['check']
+           if Yo == "sticker":
+               await message.reply_sticker(f"{hey}")
+           if not Yo == "sticker":
+               await message.reply_text(f"{hey}")
+                     
+@client.on_message(
+ (
+        filters.sticker
+        | filters.text
+    )
+    & filters.private
+    & ~filters.me
+    & ~filters.bot,
+)
+async def alexaprivatesticker(client: Client, message: Message):
+
+   chatdb = MongoClient(MONGO_URL)
+   chatai = chatdb["Word"]["WordDb"] 
+   if not message.reply_to_message:
+       await client.send_chat_action(message.chat.id, "typing")
+       K = []  
+       is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
+       for x in is_chat:
+           K.append(x['text'])
+       hey = random.choice(K)
+       is_text = chatai.find_one({"text": hey})
+       Yo = is_text['check']
+       if Yo == "text":
+           await message.reply_text(f"{hey}")
+       if not Yo == "text":
+           await message.reply_sticker(f"{hey}")
+   if message.reply_to_message:            
+       getme = await client.get_me()
+       user_id = getme.id       
+       if message.reply_to_message.from_user.id == user_id:                    
+           await client.send_chat_action(message.chat.id, "typing")
+           K = []  
+           is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
+           for x in is_chat:
+               K.append(x['text'])
+           hey = random.choice(K)
+           is_text = chatai.find_one({"text": hey})
+           Yo = is_text['check']
+           if Yo == "text":
+               await message.reply_text(f"{hey}")
+           if not Yo == "text":
+               await message.reply_sticker(f"{hey}")
+               
+
+client.run()
